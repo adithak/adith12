1148dbee4a2ab598726d9d5139d045d38b64a98f
adithak <adithak59@gmail.com>
2017-06-30 14:17:55 +0530

    Create natura.c

---
+++ natura.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i ,sum=0,n;
+clrscr();
+printf("\nenter the natural number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+sum=sum+i;
+printf("\nthe sum of natural numbers");
+getch();
+}

66e006810275af0d5cc0903b6d1665bb821fe38d
adithak <adithak59@gmail.com>
2017-06-30 13:35:58 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+char a[50];
+clrscr();
+printf("enter the charracter");
+scanf("%s",a);
+for(i=0;i<=4;i++)
+printf("\n%s",a);
+getch();
+}
